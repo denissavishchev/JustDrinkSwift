5a0790e79dee129f3029d19bc177f390cccd7951
Denis Savishchev <denis.savishchev@gmail.com>
2024-11-08 07:13:18 +0100

    Initial Commit

---
+++ JustDrink/JustDrinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustDrinkApp.swift
+//  JustDrink
+//
+//  Created by Devis on 08/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct JustDrinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
